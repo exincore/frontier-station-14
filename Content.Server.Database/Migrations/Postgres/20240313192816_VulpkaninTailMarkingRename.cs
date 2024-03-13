53e8df823486cf55fa88e2ddcadf492b28fb310d
exin <me@exin.xyz>
2024-03-13 14:33:56 -0500

    database!: Add migration to change characters of vulpkanin species to use non-animated tail markings
    This migration is backwards compatible but cannot be undone since it removes information.
    
    BREAKING CHANGE: Database migration converts vulpkanin characters to using non-animated tail markings

---
+++ Content.Server.Database/Migrations/Postgres/20240313192816_VulpkaninTailMarkingRename.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Content.Server.Database.Migrations.Postgres
+{
+    /// <inheritdoc />
+    public partial class VulpkaninTailMarkingRename : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+            @"
+                UPDATE profile
+                SET markings = REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(markings,
+                    'VulpTailWag', 'VulpTail'),
+                    'VulpTailWagTip', 'VulpTailTip'),
+                    'VulpTailFoxWag', 'VulpTailFox'),
+                    'VulpTailFoxWagTip', 'VulpTailFoxTip'),
+                    'VulpTailBushyWag', 'VulpTailBushy'),
+                    'VulpTailCoyoteWag', 'VulpTailCoyote'),
+                    'VulpTailCorgiWag', 'VulpTailCorgi'),
+                    'VulpTailDalmationWag', 'VulpTailDalmation')
+                WHERE species = 'Vulpkanin';
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
